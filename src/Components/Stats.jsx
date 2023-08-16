ddcbff653266e9c68e642bfeaf63d7b87a7f46a2
Quojo_Steve <quojosteve@gmail.com>
2023-08-16 08:22:04 +0000

    Dashboard complete

--- src/Components/Stats.jsx
+++ src/Components/Stats.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { IoBagHandle } from "react-icons/io5";
+import { IoBagHandle, IoPieChart, IoPeople, IoCart } from "react-icons/io5";
 
 const Stats = () => {
   return (
-    <div className="flex gap-4 w-full">
+    <div className="flex lg:gap-4 gap-1 md:gap-3 w-full flex-wrap">
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-sky-500">
           <IoBagHandle className="text-2xl text-white" />
@@ -20,7 +20,7 @@ const Stats = () => {
       </BoxWrapper>
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-orange-600">
-          <IoBagHandle className="text-2xl text-white" />
+          <IoPieChart className="text-2xl text-white" />
         </div>
         <div className="pl-4">
           <span className="text-sm text-gray-500 font-light">Total Expenses</span>
@@ -34,7 +34,7 @@ const Stats = () => {
       </BoxWrapper>
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-yellow-400">
-          <IoBagHandle className="text-2xl text-white" />
+          <IoPeople className="text-2xl text-white" />
         </div>
         <div className="pl-4">
           <span className="text-sm text-gray-500 font-light">Total Customers</span>
@@ -48,7 +48,7 @@ const Stats = () => {
       </BoxWrapper>
       <BoxWrapper>
         <div className="rounded-full h-12 w-12 flex items-center justify-center bg-green-600">
-          <IoBagHandle className="text-2xl text-white" />
+          <IoCart className="text-2xl text-white" />
         </div>
         <div className="pl-4">
           <span className="text-sm text-gray-500 font-light">Total Orders</span>
--- src/Pages/Items.jsx
+++ src/Pages/Items.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
 import Sidebar from "../Components/Sidebar";
+import Headers from "../Components/Headers";
+import Stats from "../Components/Stats";
+import TransactionCharts from "../Components/TransactionCharts";
+import BuyerProfileChart from "../Components/BuyerProfileChart";
+import RecentOrders from "../Components/RecentOrders";
+import PopularProducts from "../Components/PopularProducts";
 
 const Items = () => {
   return (
-    <div className="flex">
-      <Sidebar />
-      <div className="p-7 text-2xl font-semibold flex-1 h-screen">Pages</div>
+    <div className="">
+      <div className="flex bg-gray-100 overflow-hidden h-screen w-screen  flex-row ">
+        <Sidebar />
+        <div className="p-4 pt-0 text-2xl font-semibold flex-1 h-screen overflow-y-scroll">
+          <Headers />
+          <div className="flex flex-col gap-4 flex-wrap">
+            <Stats />
+            <div className="flex flex-row gap-4 w-full flex-wrap ">
+              <TransactionCharts />
+              <BuyerProfileChart />
+            </div>
+            <div className="flex flex-row gap-4 w-full flex-wrap">
+              <RecentOrders />
+              <PopularProducts />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
